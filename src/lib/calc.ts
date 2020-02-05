eb287cb99cb4c3a5b280e871bd7fe45634996632
고정완 (Can) <jw.gho@ecubelabs.com>
2020-02-05 15:54:12 +0900

    Fix algorithm: getTotalPrice 알고리즘의 할인율을 낮춤

--- src/lib/calc.ts
+++ src/lib/calc.ts
@@ -20,48 +20,43 @@ export const getTotalPrice = (
     );
   }
 
-  const couponAvailableProducts: ProductWithQuantity[] = [];
-  const couponUnavailableProducts: ProductWithQuantity[] = [];
-
+  let couponAvailableTotalPrice = 0;
+  let couponUnavailableTotalPrice = 0;
   for (const productWithQuantity of products) {
     if (productWithQuantity.product.availableCoupon === false) {
-      couponUnavailableProducts.push(productWithQuantity);
+      const {
+        quantity,
+        product: { price }
+      } = productWithQuantity;
+      couponUnavailableTotalPrice += quantity * price;
     } else {
-      couponAvailableProducts.push(productWithQuantity);
+      const {
+        quantity,
+        product: { price }
+      } = productWithQuantity;
+      couponAvailableTotalPrice += quantity * price;
     }
   }
 
-  const couponUnavailableTotalPrice = couponUnavailableProducts.reduce(
-    (accTotalPrice, { quantity, product: { price } }) =>
-      accTotalPrice + quantity * price,
-    0
-  );
-
-  if (couponAvailableProducts.length < 1) {
+  if (couponAvailableTotalPrice <= 0) {
     return couponUnavailableTotalPrice;
   }
 
-  const couponAvailableTotalPrice = couponAvailableProducts.reduce(
-    (accTotalPrice, { quantity, product: { price } }) =>
-      accTotalPrice + quantity * price,
-    0
-  );
+  const discountRates: number[] = [];
+  for (const { discountRate, discountAmount } of coupons) {
+    couponAvailableTotalPrice -= discountAmount || 0;
+    if (couponAvailableTotalPrice <= 0) {
+      return couponUnavailableTotalPrice;
+    }
+    if (discountRate && discountRate > 0 && discountRate < 100) {
+      discountRates.push(discountRate);
+    }
+  }
 
-  const totalDiscount = coupons.reduce(
-    (accDiscount, { discountRate, discountAmount }) => {
-      if (discountAmount) {
-        accDiscount += discountAmount;
-      }
-      if (discountRate && discountRate > 0 && discountRate < 100) {
-        accDiscount += couponAvailableTotalPrice * discountRate * 0.01;
-      }
-      return accDiscount;
-    },
-    0
-  );
+  for (const discountRate of discountRates.sort((num1, num2) => num1 - num2)) {
+    couponAvailableTotalPrice -=
+      couponAvailableTotalPrice * discountRate * 0.01;
+  }
 
-  const totalPrice =
-    couponUnavailableTotalPrice + couponAvailableTotalPrice - totalDiscount;
-
-  return totalPrice > 0 ? totalPrice : 0;
+  return couponUnavailableTotalPrice + couponAvailableTotalPrice;
 };
